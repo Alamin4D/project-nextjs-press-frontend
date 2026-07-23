b6c356b4a0f5e8dc23bb4073fc29f29da78234bc
Alamin4D <alaminahmed79000@gmail.com>
2026-07-23 13:16:08 +0600

    refactor: improve news listing and response handling

--- app/(public)/_actions/subscribePremium.ts
+++ app/(public)/_actions/subscribePremium.ts
@@ -21,6 +21,7 @@ export const subscribePremium = async () => {
                 Cookie: `accessToken=${accessToken}`
             }
         });
+        
     
         const result = await res.json();
     
